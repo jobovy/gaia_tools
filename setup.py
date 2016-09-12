536bd3b87e215d616bcf347dcd6081ef20783e5f
Jo Bovy <bovy@astro.utoronto.ca>
2016-09-12 12:18:15 -0400

    Add setup script

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup #, Extension
+
+long_description = "Tools for working with the @ESAGaia data and related data sets; see `here <https://github.com/jobovy/gaia_tools>`__ for further documentation"
+
+setup(name='gaia_tools',
+      version='0.1',
+      description='Gaia data tools',
+      author='Jo Bovy',
+      author_email='bovy@astro.utoronto.ca',
+      license='MIT',
+      long_description=long_description,
+      url='https://github.com/jobovy/gaia_tools',
+      package_dir = {'gaia_tools/': ''},
+      packages=['gaia_tools','gaia_tools/load'],
+      install_requires=['numpy','fitsio']
+      )
