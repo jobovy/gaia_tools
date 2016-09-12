b00a75ed2619da5dd421733c2eba35a04bf2a040
Jo Bovy <bovy@astro.utoronto.ca>
2016-09-12 16:55:14 -0400

    Docs for GALAH load function

--- gaia_tools/load/__init__.py
+++ gaia_tools/load/__init__.py
@@ -3,6 +3,18 @@ import numpy
 import astropy.io.ascii
 from gaia_tools.load import path, download
 def galah(dr=1):
+    """
+    NAME:
+       galah
+    PURPOSE:
+       Load the GALAH data
+    INPUT:
+       dr= (1) data release
+    OUTPUT:
+       data table
+    HISTORY:
+       2016-09-12 - Written - Bovy (UofT)
+    """
     filePath, ReadMePath= path.galahPath(dr=dr)
     if not os.path.exists(filePath):
         download.galah(dr=dr)
