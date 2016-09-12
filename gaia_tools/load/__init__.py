187026ce695dee79c4897c0e8e014bb208de5a83
Jo Bovy <bovy@astro.utoronto.ca>
2016-09-12 13:29:21 -0400

    Set fill value of GALAH RA and Dec explicitly

--- gaia_tools/load/__init__.py
+++ gaia_tools/load/__init__.py
@@ -1,4 +1,5 @@
 import os, os.path
+import numpy
 import astropy.io.ascii
 from gaia_tools.load import path, download
 def galah(dr=1):
@@ -6,4 +7,6 @@ def galah(dr=1):
     if not os.path.exists(filePath):
         download.galah(dr=dr)
     data= astropy.io.ascii.read(filePath,readme=ReadMePath)
+    data['RA']._fill_value= numpy.array([-9999.99])
+    data['dec']._fill_value= numpy.array([-9999.99])
     return data
