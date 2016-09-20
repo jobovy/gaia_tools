4240164a957ed7660f8230c4e962a14527fa2b1e
Jo Bovy <bovy@astro.utoronto.ca>
2016-09-20 23:15:26 +0200

    Take proper motion into account in cross-match

--- gaia_tools/xmatch/__init__.py
+++ gaia_tools/xmatch/__init__.py
@@ -8,21 +8,26 @@ import numpy
 import astropy.coordinates as acoords
 from astropy import units as u
 def xmatch(cat1,cat2,maxdist=2,
-           colRA1='RA',colDec1='DEC',
-           colRA2='RA',colDec2='DEC'):
+           colRA1='RA',colDec1='DEC',epoch1=2000.,
+           colRA2='RA',colDec2='DEC',epoch2=2000.,
+           colpmRA2='pmra',colpmDec2='pmdec'):
     """
     NAME:
        xmatch
     PURPOSE:
-       cross-match two catalogs
+       cross-match two catalogs (incl. proper motion in cat2 if epochs are different)
     INPUT:
        cat1 - First catalog
        cat2 - Second catalog
        maxdist= (2) maximum distance in arcsec
-       colRA1= ('RA') name of the tag in cat with the right ascension in cat1
-       colDec1= ('DEC') name of the tag in cat with the declination in cat1
-       colRA2= ('RA') name of the tag in cat with the right ascension in cat2
-       colDec2= ('DEC') name of the tag in cat with the declination in cat2
+       colRA1= ('RA') name of the tag in cat1 with the right ascension in degree in cat1 (assumed to be ICRS)
+       colDec1= ('DEC') name of the tag in cat1 with the declination in degree in cat1 (assumed to be ICRS)
+       epoch= (2000.) epoch of the coordinates in cat1
+       colRA2= ('RA') name of the tag in cat2 with the right ascension in degree in cat2 (assumed to be ICRS)
+       colDec2= ('DEC') name of the tag in cat2 with the declination in degree in cat2 (assumed to be ICRS)
+       epoch= (2000.) epoch of the coordinates in cat2
+       colpmRA2= ('pmra') name of the tag in cat2 with the proper motion in right ascension in degree in cat2 (assumed to be ICRS; includes cos(Dec)) [only used when epochs are different]
+       colpmDec2= ('pmdec') name of the tag in cat2 with the proper motion in declination in degree in cat2 (assumed to be ICRS) [only used when epochs are different]
     OUTPUT:
        (index into cat1 of matching objects,
         index into cat2 of matching objects,
@@ -30,8 +35,19 @@ def xmatch(cat1,cat2,maxdist=2,
     HISTORY:
        2016-09-12 - Written - Bovy (UofT)
     """
-    mc1= acoords.ICRS(cat1[colRA1],cat1[colDec1],unit=(u.degree, u.degree))
-    mc2= acoords.ICRS(cat2[colRA2],cat2[colDec2],unit=(u.degree, u.degree))
+    depoch= epoch2-epoch1
+    if depoch != 0.:
+        # Use proper motion to get both catalogs at the same time
+        dra=cat2[colpmRA2]/numpy.cos(cat2[colDec2]/180.*numpy.pi)\
+            /3600000.*depoch
+        ddec= cat2[colpmDec2]/3600000.*depoch
+    else:
+        dra= 0.
+        ddec= 0.
+    mc1= acoords.SkyCoord(cat1[colRA1],cat1[colDec1],
+                          unit=(u.degree, u.degree),frame='icrs')
+    mc2= acoords.SkyCoord(cat2[colRA2]-dra,cat2[colDec2]-ddec,
+                          unit=(u.degree, u.degree),frame='icrs')
     idx,d2d,d3d = mc1.match_to_catalog_sky(mc2)
     m1= numpy.arange(len(cat1))
     mindx= d2d < maxdist*u.arcsec
