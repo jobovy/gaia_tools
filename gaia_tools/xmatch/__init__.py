02712cc238999283c8d311501388193b62804302
henrysky <henryskyleung@gmail.com>
2018-12-18 00:24:57 -0500

    fix fp128 issue on windows, fix readme

--- gaia_tools/xmatch/__init__.py
+++ gaia_tools/xmatch/__init__.py
@@ -5,6 +5,8 @@ import shutil
 import tempfile
 import warnings
 import subprocess
+import platform
+WIN32= platform.system() == 'Windows'
 import numpy
 import astropy.coordinates as acoords
 from astropy.table import Table
@@ -181,9 +183,22 @@ inner join tap_upload.my_table as m on m.source_id = g.source_id""",
     return (ma,mai)
 
 def cds_load(filename):
-    return numpy.genfromtxt(filename,delimiter=',',skip_header=0,
-                            filling_values=-9999.99,names=True,
-                            dtype='float128')
+    if WIN32:
+        # windows do not have float128, but source_id is double
+        # get around this by squeezing precision from int64 on source_id as source_id is always integer anyway
+        # first read everything as fp64 and then convert source_id to int64 will keep its precision
+        data = numpy.genfromtxt(filename,delimiter=',',skip_header=0,
+                                filling_values=-9999.99,names=True,
+                                dtype='float64')
+        to_list = list(data.dtype.names)
+        to_list.remove('source_id')
+        # construct a list where everything is fp64 except 'source_id' being int64
+        dtype = [('{}'.format(i), numpy.float64) for i in to_list] + [('source_id', numpy.int64)]
+        return data.astype(dtype)
+    else:
+        return numpy.genfromtxt(filename,delimiter=',',skip_header=0,
+                                filling_values=-9999.99,names=True,
+                                dtype='float128')
 
 def cds_matchback(cat,xcat,colRA='RA',colDec='DEC',selection='best',
                   epoch=2000.,colpmRA='pmra',colpmDec='pmdec',):
